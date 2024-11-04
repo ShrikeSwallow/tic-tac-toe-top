8824376160ea89777e61f4e278a56c73ace1d0a2
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-04 05:34:29 +0000

    Implement complete game loop globally

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const gameboard = (() => {
-  const board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  const board = ["", "", "", "", "", "", "", "", ""];
   const fillSpace = (position, symbol) => {
     board[position] = symbol;
   };
-  return { board, fillSpace };
+  const findWinner = (symbol) => {
+    const winCon = symbol + symbol + symbol;
+    const sequence = [
+      board[0] + board[1] + board[2],
+      board[3] + board[4] + board[5],
+      board[6] + board[7] + board[8],
+      board[0] + board[3] + board[6],
+      board[1] + board[4] + board[7],
+      board[2] + board[5] + board[8],
+      board[0] + board[4] + board[8],
+      board[2] + board[4] + board[6],
+    ];
+    if (sequence.includes(winCon)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return { board, fillSpace, findWinner };
 })();
 
 const display = (() => {
@@ -30,10 +48,20 @@ const createPlayer = (name, symbol) => {
 const player1 = createPlayer("mac", "x");
 const player2 = createPlayer("ben", "o");
 
+let isWinner = false;
 display.draw(gameboard.board);
-for (let i = 0; i < 5; i++) {
+while (!isWinner) {
   player1.turn();
+  isWinner = gameboard.findWinner(player1.symbol);
   display.draw(gameboard.board);
+  if (isWinner) {
+    alert(`${player1.name} is a winner!`);
+    break;
+  }
   player2.turn();
+  isWinner = gameboard.findWinner(player2.symbol);
   display.draw(gameboard.board);
+  if (isWinner) {
+    alert(`${player2.name} is a winner!`);
+  }
 }
